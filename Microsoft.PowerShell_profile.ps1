6843e888b327250edc928763dc8c202bddd21896
chris <1373858+sciencectn@users.noreply.github.com>
2024-06-29 16:28:57 -0700

    change profile name, import pscx

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,4 @@ New-Alias -Name "subl" -Value "C:\Program Files\Sublime Text\subl.exe"
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
 Import-Module git-aliases -DisableNameChecking
+Import-Module Pscx
