772fae35f354e893437257b5c569b5b72286c9d0
Chris Taylor <199846648+taylor-sb@users.noreply.github.com>
2025-08-01 07:34:06 -0700

    update for linux

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,13 @@
 oh-my-posh init pwsh --config "$PSScriptRoot\powerlevel10k_lean.mine.json" | Invoke-Expression
 Import-Module ZLocation
 New-Alias -Name "j" Invoke-ZLocation
-New-Alias -Name "subl" -Value "C:\Program Files\Sublime Text\subl.exe"
+if($IsWindows){
+    New-Alias -Name "subl" -Value "C:\Program Files\Sublime Text\subl.exe"
+}
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
 Import-Module git-aliases -DisableNameChecking
-if(-not $IsMacOS) {
+if($IsWindows) {
     Import-Module Pscx
 }
 
--- Setup.ps1
+++ Setup.ps1
@@ -4,7 +4,18 @@ if ($PSVersionTable.PSVersion.Major -lt 7) {
     Write-Error "Your powershell version of $($PSVersionTable.PSVersion) is too old. Upgrade to at least 7.0"
     exit 1
 }
-winget install JanDeDobbeleer.OhMyPosh -s winget
+if ($IsWindows) {
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+}
+elseif ($IsMacOS) {
+    brew install jandedobbeleer/oh-my-posh/oh-my-posh
+}
+elseif ($IsLinux) {
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+}
+else {
+    Write-Error "Unsupported OS"
+}
 oh-my-posh font install Hack
 Install-Module ZLocation -Scope CurrentUser -Force
 Install-Module git-aliases -Scope CurrentUser -AllowClobber -Force
